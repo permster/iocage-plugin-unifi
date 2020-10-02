8d8bba860674b7e64a2daee731a19dd073d286f1
Travis <tjgollih@gmail.com>
2020-10-01 22:03:28 -0700

    Remove fstab mounting from post_install.sh.

--- post_install.sh
+++ post_install.sh
@@ -4,24 +4,6 @@
 VERSION=$(curl -Ls http://www.ui.com/downloads/unifi/debian/dists/stable/ubiquiti/binary-amd64/Packages.gz | zcat | grep Version: | grep -Eo '[0-9\.]+' | head -n 1)
 UNIFI_SOFTWARE_URL="https://dl.ubnt.com/unifi/${VERSION}/UniFi.unix.zip"
 
-# Add the fstab entries apparently required for OpenJDKse:
-if [ $(grep -c fdesc /etc/fstab) -eq 0 ]; then
-  echo -n "Adding fdesc filesystem to /etc/fstab..."
-  echo -e "fdesc\t\t\t/dev/fd\t\tfdescfs\trw\t\t0\t0" >> /etc/fstab
-  echo " done."
-fi
-
-if [ $(grep -c proc /etc/fstab) -eq 0 ]; then
-  echo -n "Adding procfs filesystem to /etc/fstab..."
-  echo -e "proc\t\t\t/proc\t\tprocfs\trw\t\t0\t0" >> /etc/fstab
-  echo " done."
-fi
-
-# Run mount to mount the two new filesystems:
-echo -n "Mounting new filesystems..."
-/sbin/mount -a
-echo " done."
-
 # Switch to a temp directory for the Unifi download:
 cd `mktemp -d -t unifi`
 
